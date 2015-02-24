ef4cca9b6e391e4fb1f469d66873a8f0db182c22
Ido Feins <ifeins@gmail.com>
2015-02-24 16:41:57 +0200

    a little rename.

--- app/src/main/java/com/iic/lunchtime/services/LunchFetcherService.java
+++ app/src/main/java/com/iic/lunchtime/services/LunchFetcherService.java
@@ -6,14 +6,14 @@ import android.content.Intent;
 /**
  * Created by ifeins on 2/24/15.
  */
-public class LunchFetcher extends IntentService {
+public class LunchFetcherService extends IntentService {
 
-  private static final String SERVICE_NAME = "LunchFetcher";
+  private static final String SERVICE_NAME = "LunchFetcherService";
 
   /**
    * Creates an IntentService.  Invoked by your subclass's constructor.
    */
-  public LunchFetcher() {
+  public LunchFetcherService() {
     super(SERVICE_NAME);
   }
 
